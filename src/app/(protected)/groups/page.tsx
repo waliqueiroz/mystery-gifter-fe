8b4b1990b6dba46f79e6527905100921fc5e0661
Wali Queiroz <wsantos077@gmail.com>
2026-05-02 22:45:13 -0300

    feat(T015): wrap protected layout with ToastProvider and add groups page

---
+++ src/app/(protected)/groups/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import AuthGuard from '@/components/auth/AuthGuard'
+import { GroupList } from '@/components/groups/GroupList/GroupList'
+import { getUser } from '@/lib/session'
+
+export default function GroupsPage() {
+  const user = getUser()
+
+  return (
+    <AuthGuard>
+      <div className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1 className="m-0" style={{ color: 'var(--mg-text)' }}>
+                Grupos
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="content">
+        <div className="container-fluid">
+          {user && <GroupList userId={user.id} />}
+        </div>
+      </section>
+    </AuthGuard>
+  )
+}
